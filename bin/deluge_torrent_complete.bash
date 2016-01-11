fc067ef29f3fb98d58ab5213ea03522741d03fab
sisfs <m.tallant+github@gmail.com>
2016-01-10 22:56:46 -0500

    Added traking to complete/added runs
    now each script will write a line into execute_script to signify tha thte script has run.

--- bin/deluge_torrent_complete.bash
+++ bin/deluge_torrent_complete.bash
@@ -1,3 +1,6 @@
 #!/usr/local/bin/bash
-
+torrentid=$1
+torrentname=$2
+torrentpath=$3
+echo "Torrent Complete: " $torrentname $torrentpath $torrentid  >> /home/deluge/.flexget/bin/execute_script.txt
 /usr/local/bin/python2.7 /home/deluge/.flexget/bin/deluge_torrent_complete $1 $2 $3
