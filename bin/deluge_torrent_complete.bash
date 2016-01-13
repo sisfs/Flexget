4a4a7eefa34551efe8cf386ebfb6a2c5a50f58c3
sarethan <russelltking@gmail.com>
2016-01-13 18:57:45 -0500

    Manuel Change to Sisfs---ask Sisfs on this file

---
+++ bin/deluge_torrent_complete.bash
@@ -0,0 +1,6 @@
+#!/usr/local/bin/bash
+torrentid=$1
+torrentname=$2
+torrentpath=$3
+echo "Torrent Complete: " $torrentname $torrentpath $torrentid  >> /var/lib/deluge/.flexget/bin/execute_script.txt
+/usr/local/bin/python2.7 /var/lib/deluge/.flexget/bin/deluge_torrent_complete $1 $2 $3
